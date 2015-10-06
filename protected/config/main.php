cbce6a1af94095b163c53d812a3e00ae9728fff9
Miller Stiven <stiven-0611@hotmail.com>
2015-10-06 14:38:22 -0500

    Modificación CRUD
    Mejoras a la funcionalidad del crud y el aspecto grafico del directorio
    users

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
+	
 	'theme'=>"classic",
 	// preloading 'log' component
 	'preload'=>array('log'),
--- protected/views/users/_form.php
+++ protected/views/users/_form.php
@@ -15,26 +15,26 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
+	<div class>
 		<?php echo $form->labelEx($model,'username'); ?>
 		<?php echo $form->textField($model,'username',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'username'); ?>
 	</div>
 
-	<div class="row">
+	<div class>
 		<?php echo $form->labelEx($model,'password'); ?>
 		<?php echo $form->passwordField($model,'password',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'password'); ?>
 	</div>
 
-	<div class="row">
+	<div class>
 		<?php echo $form->labelEx($model,'email'); ?>
 		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'email'); ?>
 	</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	<div class="buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array("class"=>"btn btn-primary btn-large")); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
